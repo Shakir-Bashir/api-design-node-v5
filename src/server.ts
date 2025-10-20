9a3dd72ca3ba70987ca6243ec4950ea7cc894f32
Shakir-Bashir <shakir.786.external@gmail.com>
2025-10-20 08:13:07 +0530

    complete lec 03

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from 'express'
 
+import authRoutes from './route/authRoutes.ts'
+import userRoutes from './route/userRoutes.ts'
+import habitRoutes from './route/habitRoutes.ts'
+
 const app = express()
 
 app.get('/health', (req, res) => {
   res.send('<button>Click</button>')
 })
+
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/habits', habitRoutes)
 
 export { app }
 
